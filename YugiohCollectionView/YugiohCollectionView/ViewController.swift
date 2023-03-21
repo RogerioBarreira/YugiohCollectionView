88632854cdcc2018f61205455108500fd5f2383f
Rogerio Barreira <abacuque37@hotmail.com>
2023-03-20 22:57:28 -0300

    Project Template Creation

---
+++ YugiohCollectionView/YugiohCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YugiohCollectionView
+//
+//  Created by Rogerio Martins on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
