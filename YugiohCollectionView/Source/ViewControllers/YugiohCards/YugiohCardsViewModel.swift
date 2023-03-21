511399e8d4c854ee7fcc714835cf0fdf70b970be
Rogerio Barreira <abacuque37@hotmail.com>
2023-03-21 11:43:51 -0300

    Message Error

--- YugiohCollectionView/Source/ViewControllers/YugiohCards/YugiohCardsViewModel.swift
+++ YugiohCollectionView/Source/ViewControllers/YugiohCards/YugiohCardsViewModel.swift
@@ -9,4 +9,18 @@ import Foundation
 
 class YugiohCardsViewModel: NSObject {
     
+    private let request = Request()
+    private var myCards: YugiohCards?
+    
+    func requestYugiohCards(completion: @escaping(Bool)-> Void) {
+        request.requestYugiohCards { [weak self] myCard, success in
+            guard let self = self else { return }
+            if success {
+                self.myCards = myCard
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
 }
